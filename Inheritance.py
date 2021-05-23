790590b061507d16583cd5a5f2e536c9e2a2c4cb
arheonix <vasiliev1998@gmail.com>
2021-05-23 22:26:57 +0300

    commit on 'main'

--- Inheritance.py
+++ Inheritance.py
@@ -13,17 +13,88 @@ class LogicGate:
 
 class BinaryGate(LogicGate):
 
-    def __init__(self, n):
+    def __init__(self,n):
         LogicGate.__init__(self,n)
 
         self.pinA = None
         self.pinB = None
 
     def getPinA(self):
-        return int(input("Enter Pin A input for gate "+ self.getLabel()+"-->"))
+        if self.pinA == None:
+            return int(input("Enter Pin A input for gate "+self.getLabel()+"-->"))
+        else:
+            return self.pinA.getFrom().getOutput()
 
     def getPinB(self):
-        return int(input("Enter Pin B input for gate "+ self.getLabel()+"-->"))
+        if self.pinB == None:
+            return int(input("Enter Pin B input for gate "+self.getLabel()+"-->"))
+        else:
+            return self.pinB.getFrom().getOutput()
+
+    def setNextPin(self,source):
+        if self.pinA == None:
+            self.pinA = source
+        else:
+            if self.pinB == None:
+                self.pinB = source
+            else:
+                raise RuntimeError("NO EMPTY PINS")
+
+class AndGate(BinaryGate):
+
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+class NandGate(BinaryGate):
+
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == b:
+            return 1
+        else:
+            return 0
+
+class OrGate(BinaryGate):
+
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 or b == 1:
+            return 1
+        else:
+            return 0
+
+class NorGate(BinaryGate):
+
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == b:
+            return 0
+        else:
+            return 1
 
 class UnaryGate(LogicGate):
 
@@ -33,18 +104,48 @@ class UnaryGate(LogicGate):
         self.pin = None
 
     def getPin(self):
-        return int(input("Enter Pin input for gate "+ self.getLabel()+"-->"))
+        if self.pin == None:
+            return int(input("Enter Pin input for gate "+self.getLabel()+"-->"))
+        else:
+            return self.pin.getFrom().getOutput()
 
-class AndGate(BinaryGate):
+    def setNextPin(self,source):
+        if self.pin == None:
+            self.pin = source
+        else:
+            raise RuntimeError("NO EMPTY PINS")
+
+class NotGate(UnaryGate):
 
     def __init__(self,n):
-        BinaryGate.__init__(self,n)
-    
-    def performGateLogic(self):
+        UnaryGate.__init__(self,n)
 
-        a = self.getPinA()
-        b = self.getPinB()
-        if a == 1 and b == 1:
-            return 1
-        else:
+    def performGateLogic(self):
+        if self.getPin():
             return 0
+        else:
+            return 1
+
+class Connector:
+
+    def __init__(self, fgate, tgate):
+        self.fromgate = fgate
+        self.togate = tgate
+
+        tgate.setNextPin(self)
+
+    def getFrom(self):
+        return self.fromgate
+
+    def getTo(self):
+        return self.togate
+
+def main():
+   g1 = NandGate("G1")
+   g2 = NandGate("G2")
+   g3 = AndGate("G3")
+   c1 = Connector(g1,g3)
+   c2 = Connector(g2,g3)
+   print(g3.getOutput())
+
+main()
