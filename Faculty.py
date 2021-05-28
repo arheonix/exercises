ae821836f16f69c418bdde673618a9a0704a0dd2
arheonix <vasiliev1998@gmail.com>
2021-05-28 16:50:34 +0300

    commit to 'main'

--- Faculty.py
+++ Faculty.py
@@ -13,7 +13,7 @@ class Student(Person):
         self.mark = mark
 
     def get_info(self):
-        print(self.name + ', ' + self.age + ', учится на ' + self.year + ' курсе и имеет средний балл ' + self.mark)
+        print('{}, {}, учится на {} курсе и имеет средний балл {}'.format(self.name, self.age, self.year, self.mark))
 
 
 class Teacher(Person):
@@ -24,8 +24,8 @@ class Teacher(Person):
         self.title = title
 
     def get_info(self):
-        print(self.name + ', ' + self.age + ', преподает на кафедре "' + self.department + '" и имеет ученую степень '
-              + self.title)
+        print('{}, {}, преподает на кафедре "{}" и имеет ученую степень {}'.format(self.name, self.age, self.department,
+                                                                                   self.title))
 
 
 class Dean(Person):
@@ -35,19 +35,19 @@ class Dean(Person):
         self.faculty = faculty
 
     def get_info(self):
-        print(self.name + ', ' + self.age + ', занимает должность декана на факультете "' + self.faculty + '"')
+        print('{}, {}, занимает должность декана на факультете "{}"'.format(self.name, self.age, self.faculty))
 
 # -------------------------------------------------
 # Пример выполнения кода:
 #
 # def main():
-#    s1 = Student("Вася", "22", "3", "3.5")
-#    s2 = Student("Маша", "19", "2", "4.8")
-#    t1 = Teacher("Анатолий", "45", "Механика", "к.т.н")
-#    d1 = Dean("Пётр", "56", "Математика")
+#    s1 = Student("Вася", 22, 3, 3.5)
+#    s2 = Student("Маша", 19, 2, 4.8)
+#    t1 = Teacher("Анатолий", 45, "Механика", "к.т.н")
+#    d1 = Dean("Пётр", 56, "Математика")
 #
 #    s1.get_info()
-#   s2.get_info()
+#    s2.get_info()
 #    t1.get_info()
 #    d1.get_info()
 #
